d28a49fa7341ea2e683c5d588270a4fb61399b94
AdrianHaubrich <adrianluca13@gmail.com>
2021-08-15 17:42:34 +0200

    Framework init

--- Package.swift
+++ Package.swift
@@ -5,10 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "HelenaWeather",
+    platforms: [.iOS(.v14)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "HelenaWeather",
+            type: .dynamic,
             targets: ["HelenaWeather"]),
     ],
     dependencies: [
